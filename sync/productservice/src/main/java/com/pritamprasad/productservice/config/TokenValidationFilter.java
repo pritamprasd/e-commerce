842cb756f323f0f99fff4ffebbd0e405a1a39216
Pritam Prasad <pritam.prasd@gmail.com>
2019-12-10 19:12:35 +0530

    Added Service Discovery for product service to authservice communication

--- sync/productservice/src/main/java/com/pritamprasad/productservice/config/TokenValidationFilter.java
+++ sync/productservice/src/main/java/com/pritamprasad/productservice/config/TokenValidationFilter.java
@@ -1,7 +1,11 @@
 package com.pritamprasad.productservice.config;
 
+import com.netflix.discovery.DiscoveryClient;
+import com.netflix.discovery.EurekaClient;
+import com.netflix.discovery.shared.Application;
 import com.pritamprasad.productservice.exception.InvalidTokenException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -19,6 +23,13 @@ public class TokenValidationFilter implements Filter {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private EurekaClient discoveryClient;
+
+    @Value("${authservice.name}")
+    private String authService;
+
+
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest req = (HttpServletRequest) request;
@@ -26,7 +37,9 @@ public class TokenValidationFilter implements Filter {
             throw new InvalidTokenException("No token provided in header");
         }
         try {
-            ResponseEntity<String> reponse = restTemplate.getForEntity("http://localhost:8092/validate/" + req.getHeader("token"), String.class);
+            ResponseEntity<String> reponse = restTemplate.getForEntity(
+                    discoveryClient.getNextServerFromEureka(authService, false).getHomePageUrl()+ "validate/" + req.getHeader("token"),
+                    String.class);
             if (reponse.getStatusCode().is2xxSuccessful()) {
                 chain.doFilter(request, response);
             }
